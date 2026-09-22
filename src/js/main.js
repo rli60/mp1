24f84f0739dc8c24c7f054a91018518e1da79071
Rachel Li <rli60@illinois.edu>
2026-09-22 03:07:26 -0500

    Added projects, experience, and socials.

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,39 @@ window.addEventListener("scroll", () => {
         header.classList.remove("scrolled") 
     }
 })
+
+var modal1 = document.getElementById("myModal1");
+var btn1 = document.getElementById("myBtn1");
+var span1 = document.getElementsByClassName("close1")[0];
+
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+var modal2 = document.getElementById("myModal2");
+var btn2 = document.getElementById("myBtn2");
+var span2 = document.getElementsByClassName("close2")[0];
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
